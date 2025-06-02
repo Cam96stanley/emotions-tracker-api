0d335695c13c02d2478dd6a9203f466e614f1fb7
Cameron Stanley <cam96stanley@gmail.com>
2025-06-02 19:24:03 -0400

    prepped for deployment

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class DevelopmentConfig:
   SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
-  SECRET_KEY = os.getenv("SECRET_KEY")
+  SECRET_KEY = os.getenv("SECRET_KEY") or "super secret key"
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   CACHE_TYPE = "SimpleCache"
   
@@ -10,4 +10,8 @@ class TestingConfig:
   SQLALCHEMY_DATABASE_URI = "sqlite:///testing.db"
   SECRET_KEY = "super-secret-testing-key"
   DEBUG = True
+  CACHE_TYPE = "SimpleCache"
+  
+class ProductionConfig:
+  PROD_SQLALCHEMY_DATABASE_URI = os.environ.get("PROD_SQLALCHEMY_DATABASE_URI")
   CACHE_TYPE = "SimpleCache"
--- flask_app.py
+++ flask_app.py
@@ -5,7 +5,7 @@ from app.seeders.seed_moods import seed_moods
 
 load_dotenv()
 
-app = create_app("DevelopmentConfig")
+app = create_app("ProductionConfig")
 
 with app.app_context():
   db.create_all()
