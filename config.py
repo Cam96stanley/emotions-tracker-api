6205c4d3d4a0175cba69096c0fff2dad75845834
Cameron Stanley <cam96stanley@gmail.com>
2025-06-02 19:48:22 -0400

    updated config to only take production uri from env file

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class DevelopmentConfig:
-  SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+  SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:Cam.110196@localhost/emotion_tracker_db"
   SECRET_KEY = os.getenv("SECRET_KEY") or "super secret key"
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   CACHE_TYPE = "SimpleCache"
@@ -13,5 +13,5 @@ class TestingConfig:
   CACHE_TYPE = "SimpleCache"
   
 class ProductionConfig:
-  SQLALCHEMY_DATABASE_URI = os.environ.get("PROD_SQLALCHEMY_DATABASE_URI")
+  SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
   CACHE_TYPE = "SimpleCache"
